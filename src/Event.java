666e10bfbe8591acde84644c8138b1b30d6d9dda
Obraiel <konrad.marcinnn@gmail.com>
2023-05-24 12:57:29 +0200

    Extended Event.java & EventField.java with 'how to store events'

--- src/Event.java
+++ src/Event.java
@@ -1,7 +1,17 @@
 package src;
 
+class EventType{
+    public final int moveEvent=1;
+    public final int payEvent=2;
+    public final int turnEvent=3;
+}
 public class Event {
     private String name;
+    private EventType eventType;
+    private int toPay;
+    private int fieldIndex;
+    private int turnSkipAmount;
+
 
     public String getName() {
         return name;
--- src/EventField.java
+++ src/EventField.java
@@ -1,4 +1,11 @@
 package src;
 
 public class EventField extends Field{
+    private boolean isEvent;
+
+    //Pola ForcePay
+    //pola GoDante
+    //pole więzienie
+    //pole Konkurs
+    //pole akcji
 }
