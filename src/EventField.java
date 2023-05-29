76ac0b18df2d07de40cc219ce8e05d4f3b53116b
Obraiel <konrad.marcinnn@gmail.com>
2023-05-29 21:53:32 +0200

    Extended SpecialField.java & EventField.java with 'what information they store'

--- src/EventField.java
+++ src/EventField.java
@@ -1,6 +1,12 @@
 package src;
 
 public class EventField extends Field {
+    private int stayingTurns;
+    private int travelTargetIndex;
+    private int ammountOfFieldsToGo;
+    private boolean isSaveable;
+
+
     public EventField(String fieldName) {
         this.fieldName = fieldName;
         this.canBuy = false;
