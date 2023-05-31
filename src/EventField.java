e16147bf8571f2845209fd179254557c45d3ad6e
Obraiel <konrad.marcinnn@gmail.com>
2023-05-31 12:50:43 +0200

    Update EventField.java

--- src/EventField.java
+++ src/EventField.java
@@ -1,10 +1,8 @@
 package src;
 
 public class EventField extends Field {
-    private int stayingTurns;
-    private int travelTargetIndex;
-    private int ammountOfFieldsToGo;
-    private boolean isSaveable;
+  
+
 
 
     public EventField(String fieldName) {
