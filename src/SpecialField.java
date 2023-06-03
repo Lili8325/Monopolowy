0199879ef27891a9d8455e08691cb7e45a03ad77
Obraiel <konrad.marcinnn@gmail.com>
2023-06-03 11:52:28 +0200

    Extended SpecialField.java by more information what and how it stores

--- src/SpecialField.java
+++ src/SpecialField.java
@@ -2,9 +2,9 @@ package src;
 
 public class SpecialField extends Field{
     private Event eventCard;
-    private int amountOfFieldsBougthByPlayer;
-    private int belongToPlayerNumber;
-    //tu można zrobić tak że jaeśli pole daje pieniądze czyli konkurs WRS to po ptostu na minos jak tam na innym polu dałaś
+    private int amountOfFieldsBougthByPlayer;//tu nie jestem pewien czy z tym będzie łatwiej czy trudniej bo generalnie chodzi o to żeby podstawowy czynsz mnozyć przez ilość posiadanych SpecialFieldów
+    private int belongToPlayerNumber;    //tu można zrobić tak że jaeśli pole daje pieniądze czyli konkurs WRS to po ptostu na minos jak tam na innym polu dałaś
+    private int stayingCost;
 
 
 
@@ -24,4 +24,6 @@ public class SpecialField extends Field{
     //pole więzienie
     //pole Konkurs
     //pole akcji
+
+    // I teraz też się w mojej głowie rodzi problem jak rozróżnić te akcjie od narozników więc jak będzie bardzo trudno to znaki dymne daj to zmienimy plansze żeby w mejescah tych SpecialEventów były zwykłe szanse
 }
